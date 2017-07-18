cf412b9e0c4252defea8f9f061a1b01243c8e77f
Konstantin Mihaylov <TheMustardVader@users.noreply.github.com>
2017-07-18 21:00:03 +0300

    Update P02_RandomizeWords.cs

--- L19_ObjectsAndClasses-Lab/P02_RandomizeWords/P02_RandomizeWords.cs
+++ L19_ObjectsAndClasses-Lab/P02_RandomizeWords/P02_RandomizeWords.cs
@@ -30,3 +30,44 @@ namespace P02_RandomizeWords
         }
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////////
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Globalization;
+    using System.Net;
+    using System.IO;
+
+
+    namespace P1.DateTime
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var words = Console.ReadLine().Split(' ');
+
+                var random = new Random();
+
+                var result = new List<string>();
+                for (int i = 0; i < words.Length; i++)
+                {
+                    var currentWord = words[i];
+                    var randomIndex = random.Next(0, words.Length);
+
+                    var tempWord = words[randomIndex];
+                    words[i] = tempWord;
+                    words[randomIndex] = currentWord;
+
+                }
+                foreach (var word in words)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
